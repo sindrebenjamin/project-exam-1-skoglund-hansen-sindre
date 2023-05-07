9cd72b71b75cfac4f063120799b01810263239fe
sindrebenjamin <sindskog@gmail.com>
2023-05-07 09:47:09 +0200

    fått på plass fetch, querystring og bittelitt css

---
+++ js/post.js
@@ -0,0 +1,124 @@
+//key: ]K;(3}.2J)Ej~S2a|.J|W+|c7{9_h@xts>;S&v/0A~(+s+%mZy0_*Ee.YP]25XwV
+
+const title = document.querySelector("title");
+const article = document.querySelector("article");
+const commentSection = document.querySelector(".comment-section");
+const urlParams = new URLSearchParams(window.location.search);
+const postId = urlParams.get("id");
+
+async function fetchData() {
+
+    const response = await fetch(`https://sindre.codes/bingo/wp-json/wp/v2/posts/${postId}?_embed`)
+    const result = await response.json();
+    printData(result)
+}
+
+fetchData();
+
+function printData(data) {
+
+    title.innerHTML = data.title.rendered;
+
+
+    //Category
+
+    const blogCategory = document.createElement("p");
+    blogCategory.classList.add("blog-category", "border-radius");
+    blogCategory.innerHTML = data._embedded['wp:term'][0][0].name;
+
+
+
+    //Title
+
+    const blogTitle = document.createElement("p");
+    blogTitle.classList.add("blog-title");
+    blogTitle.innerHTML = data.title.rendered;
+   
+
+
+   //Content
+
+    const blogContent = document.createElement("article");
+    blogContent.innerHTML = data.content.rendered;
+    // console.log(blogContent);
+
+    article.appendChild(blogCategory);
+    article.appendChild(blogTitle);
+    article.appendChild(blogContent);
+    
+}
+
+
+async function fetchComments() {
+    
+        const response = await fetch(`https://sindre.codes/bingo/wp-json/wp/v2/comments?post=${postId}`)
+        const result = await response.json();
+        printComments(result)
+
+}
+
+fetchComments()
+
+
+function printComments(comments) {
+console.log(comments);
+
+
+    for (let i = 0; i < comments.length; i++) {
+
+        const dateString = comments[i].date;
+        const date = new Date(dateString);
+        const options = { day: 'numeric', month: 'long', year: 'numeric' };
+        const formattedDate = date.toLocaleDateString('en-US', options);
+
+
+        const comment = document.createElement("div");
+        comment.classList.add("comment");
+        comment.innerHTML = `
+        <div class="comment-info">
+            <img src=${comments[i].author_avatar_urls[48]} alt="avatar" class="comment-avatar">
+            <p class="comment-author">${comments[i].author_name}</p>
+            <p class="comment-date">${formattedDate}</p>
+        </div>
+        <div class="comment-content">
+            <p>${comments[i].content.rendered}</p>
+        </div>
+        `
+        commentSection.appendChild(comment);
+    }
+}
+
+
+const commentForm = document.querySelector("#comment-form");
+
+commentForm.addEventListener(`submit`, async (event) => { 
+    event.preventDefault();
+
+    const name = document.querySelector("#comment-author").value;
+    const email = document.querySelector("#comment-email").value;
+    const comment = document.querySelector("#comment-content").value;
+
+    const data = {
+        author_name: name,
+        author_email: email,
+        content: comment,
+        post: postId
+    }
+
+    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiSm9obiBEb2UiLCJlbWFpbCI6ImpvaG4uZG9lQGV4YW1wbGUuY29tIiwiaXNzIjoiaHR0cHM6Ly9zaW5kcmUuY29kZXMvYmluZ28vd3AtanNvbi93cC92Mi9jb21tZW50cyJ9.Hmp6G7CWF5p5evSOSZhS9G96xi5tGxxAc06GZ-BwBt0";
+    const response = await fetch("https://sindre.codes/bingo/wp-json/wp/v2/comments", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify(data)
+    })
+
+    const result = await response.json();
+    console.log(result);
+
+    commentForm.reset();
+    fetchComments();
+
+});
---
+++ js/script.js
@@ -0,0 +1,100 @@
+// Burger menu
+
+const menuBtn = document.querySelector(".menu-btn");
+const mobileNav = document.querySelector(".mobile-nav-wrapper");
+const mobileOverlay = document.querySelector(".mobile-nav-overlay")
+const body = document.querySelector("body");
+
+mobileOverlay.onclick = function() {
+    toggleMenu();
+}
+
+menuBtn.onclick = function() {
+   toggleMenu();
+}
+
+function toggleMenu() {
+    menuBtn.classList.toggle("open");
+    mobileNav.classList.toggle("open");
+    body.classList.toggle("no-scroll");
+}
+
+
+// Blog
+
+const blogContainer = document.querySelector(".blog-card-container");
+
+async function fetchData() {
+
+    const response = await fetch("https://sindre.codes/bingo/wp-json/wp/v2/posts?_embed")
+    const result = await response.json();
+    printData(result)
+    console.log(result);
+}
+
+fetchData();
+
+
+function printData(data) {
+
+  
+
+    for (let i = 0; i < data.length; i++) {
+
+        const dateString = data[i].date;
+        const date = new Date(dateString);
+        const options = { day: 'numeric', month: 'long', year: 'numeric' };
+        const formattedDate = date.toLocaleDateString('en-US', options);
+
+        //Image
+
+        const blogImage = document.createElement("img");
+        blogImage.classList.add("blog-image");
+        blogImage.src = data[i]._embedded['wp:featuredmedia'][0].source_url;
+
+        //Category
+
+        const blogCategory = document.createElement("div");
+        blogCategory.classList.add("blog-category", "border-radius");
+        blogCategory.innerHTML = data[i]._embedded['wp:term'][0][0].name;
+        console.log(blogCategory);
+
+        //Date
+
+        const blogDate = document.createElement("p");
+        blogDate.classList.add("blog-date");
+        blogDate.innerHTML = formattedDate;
+        console.log(blogDate);
+
+        //Category-date
+
+        const blogCategoryDate = document.createElement("div");
+        blogCategoryDate.classList.add("flex", "blog-category-date");
+        blogCategoryDate.appendChild(blogCategory);
+        blogCategoryDate.appendChild(blogDate);
+        
+
+        //Title
+
+        const blogTitle = document.createElement("p");
+        blogTitle.classList.add("blog-title");
+        blogTitle.innerHTML = data[i].title.rendered;
+     
+
+        //Blog-card
+
+        const blogCard = document.createElement("a");
+        blogCard.classList.add("blog-card", "border-radius");
+        blogCard.href=`../pages/post.html?id=${data[i].id}`;
+        blogCard.appendChild(blogImage);
+        blogCard.appendChild(blogCategoryDate);
+        blogCard.appendChild(blogTitle);
+
+
+        //Post
+      
+        blogContainer.appendChild(blogCard);
+       
+    }
+
+}
